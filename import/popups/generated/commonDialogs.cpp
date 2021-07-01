c63b27b5485f7b8fae4300131f7552ad5da1cf22
David Llewellyn-Jones <david.llewellyn-jones@jolla.com>
2021-07-01 15:05:58 +0000

    [sailfish-webview] Add qtTrIds generated from gecko l10n keys. Contributes to JB#53307 OMP#JOLLA-219
    Adds localization keys from passwordmgr.properties and
    commonDialogs.properties. The files were generated using the
    generate_property_translations.py tool.
    
    Taken from the hg.mozilla.org/l10n-central commit
    0f3c06bd68fc99da1a2e724f5a78119f922a64cc . See the following two files:
    
    https://hg.mozilla.org/l10n-central/en-GB/raw-file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/passwordmgr/passwordmgr.properties
    https://hg.mozilla.org/l10n-central/en-GB/raw-file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties
    
    The commands used to generate the files were the following.
    
    generate_property_translations.py --url
    'https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/passwordmgr/passwordmgr.properties'
    passwordmgr.properties > passwordmgr.cpp
    
    generate_property_translations.py --url
    'https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties'
    commonDialogs.properties > commonDialogs.cpp

---
+++ import/popups/generated/commonDialogs.cpp
@@ -0,0 +1,152 @@
+/* Generated dummy file, do not edit */
+
+//: This is a transformed translation key, the original translation key
+//: (Alert) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 5
+//% "Alert"
+const auto alert = qtTrId("sailfish_components_webview_popups-la-alert");
+
+//: This is a transformed translation key, the original translation key
+//: (Confirm) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 6
+//% "Confirm"
+const auto confirm = qtTrId("sailfish_components_webview_popups-la-confirm");
+
+//: This is a transformed translation key, the original translation key
+//: (ConfirmCheck) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 7
+//% "Confirm"
+const auto confirm_check = qtTrId("sailfish_components_webview_popups-la-confirm_check");
+
+//: This is a transformed translation key, the original translation key
+//: (Prompt) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 8
+//% "Prompt"
+const auto prompt = qtTrId("sailfish_components_webview_popups-la-prompt");
+
+//: This is a transformed translation key, the original translation key
+//: (PromptUsernameAndPassword3) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 10
+//% "Authentication Required - %1"
+const auto prompt_username_and_password3 = qtTrId("sailfish_components_webview_popups-la-prompt_username_and_password3");
+
+//: This is a transformed translation key, the original translation key
+//: (PromptPassword3) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 12
+//% "Password Required - %1"
+const auto prompt_password3 = qtTrId("sailfish_components_webview_popups-la-prompt_password3");
+
+//: This is a transformed translation key, the original translation key
+//: (Select) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 13
+//% "Select"
+const auto select = qtTrId("sailfish_components_webview_popups-la-select");
+
+//: This is a transformed translation key, the original translation key
+//: (OK) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 14
+//% "OK"
+const auto ok = qtTrId("sailfish_components_webview_popups-la-ok");
+
+//: This is a transformed translation key, the original translation key
+//: (Cancel) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 15
+//% "Cancel"
+const auto cancel = qtTrId("sailfish_components_webview_popups-la-cancel");
+
+//: This is a transformed translation key, the original translation key
+//: (Yes) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 16
+//% "Yes"
+const auto yes = qtTrId("sailfish_components_webview_popups-la-yes");
+
+//: This is a transformed translation key, the original translation key
+//: (No) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 17
+//% "No"
+const auto no = qtTrId("sailfish_components_webview_popups-la-no");
+
+//: This is a transformed translation key, the original translation key
+//: (Save) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 18
+//% "Save"
+const auto save = qtTrId("sailfish_components_webview_popups-la-save");
+
+//: This is a transformed translation key, the original translation key
+//: (Revert) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 19
+//% "Revert"
+const auto revert = qtTrId("sailfish_components_webview_popups-la-revert");
+
+//: This is a transformed translation key, the original translation key
+//: (DontSave) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 20
+//% "Don’t Save"
+const auto dont_save = qtTrId("sailfish_components_webview_popups-la-dont_save");
+
+//: This is a transformed translation key, the original translation key
+//: (ScriptDlgGenericHeading) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 21
+//% "[JavaScript Application]"
+const auto script_dlg_generic_heading = qtTrId("sailfish_components_webview_popups-la-script_dlg_generic_heading");
+
+//: This is a transformed translation key, the original translation key
+//: (ScriptDlgHeading) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 22
+//% "The page at %1 says:"
+const auto script_dlg_heading = qtTrId("sailfish_components_webview_popups-la-script_dlg_heading");
+
+//: This is a transformed translation key, the original translation key
+//: (ScriptDlgNullPrincipalHeading) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 23
+//% "This page says:"
+const auto script_dlg_null_principal_heading = qtTrId("sailfish_components_webview_popups-la-script_dlg_null_principal_heading");
+
+//: This is a transformed translation key, the original translation key
+//: (ScriptDialogLabel) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 24
+//% "Prevent this page from creating additional dialogues"
+const auto script_dialog_label = qtTrId("sailfish_components_webview_popups-la-script_dialog_label");
+
+//: This is a transformed translation key, the original translation key
+//: (ScriptDialogLabelNullPrincipal) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 25
+//% "Don’t allow this site to prompt you again"
+const auto script_dialog_label_null_principal = qtTrId("sailfish_components_webview_popups-la-script_dialog_label_null_principal");
+
+//: This is a transformed translation key, the original translation key
+//: (ScriptDialogLabelContentPrincipal) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 29
+//% "Don’t allow %1 to prompt you again"
+const auto script_dialog_label_content_principal = qtTrId("sailfish_components_webview_popups-la-script_dialog_label_content_principal");
+
+//: This is a transformed translation key, the original translation key
+//: (ScriptDialogPreventTitle) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 30
+//% "Confirm Dialogue Preference"
+const auto script_dialog_prevent_title = qtTrId("sailfish_components_webview_popups-la-script_dialog_prevent_title");
+
+//: This is a transformed translation key, the original translation key
+//: (EnterLoginForRealm3) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 36
+//% "%2 is requesting your username and password. The site says: “%1”"
+const auto enter_login_for_realm3 = qtTrId("sailfish_components_webview_popups-la-enter_login_for_realm3");
+
+//: This is a transformed translation key, the original translation key
+//: (EnterLoginForProxy3) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 37
+//% "The proxy %2 is requesting a username and password. The site says: “%1”"
+const auto enter_login_for_proxy3 = qtTrId("sailfish_components_webview_popups-la-enter_login_for_proxy3");
+
+//: This is a transformed translation key, the original translation key
+//: (EnterUserPasswordFor2) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 38
+//% "%1 is requesting your username and password."
+const auto enter_user_password_for2 = qtTrId("sailfish_components_webview_popups-la-enter_user_password_for2");
+
+//: This is a transformed translation key, the original translation key
+//: (EnterUserPasswordForCrossOrigin2) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 39
+//% "%1 is requesting your username and password. WARNING: Your password will not be sent to the web site you are currently visiting!"
+const auto enter_user_password_for_cross_origin2 = qtTrId("sailfish_components_webview_popups-la-enter_user_password_for_cross_origin2");
+
+//: This is a transformed translation key, the original translation key
+//: (EnterPasswordFor) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 40
+//% "Enter password for %1 on %2"
+const auto enter_password_for = qtTrId("sailfish_components_webview_popups-la-enter_password_for");
+
+//: This is a transformed translation key, the original translation key
+//: (EnterCredentials) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 41
+//% "This site is asking you to sign in."
+const auto enter_credentials = qtTrId("sailfish_components_webview_popups-la-enter_credentials");
+
+//: This is a transformed translation key, the original translation key
+//: (EnterPasswordOnlyFor) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 43
+//% "This site is asking you to sign in as %1."
+const auto enter_password_only_for = qtTrId("sailfish_components_webview_popups-la-enter_password_only_for");
+
+//: This is a transformed translation key, the original translation key
+//: (EnterCredentialsCrossOrigin) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 45
+//% "This site is asking you to sign in. Warning: Your login information will be shared with %1, not the web site you are currently visiting."
+const auto enter_credentials_cross_origin = qtTrId("sailfish_components_webview_popups-la-enter_credentials_cross_origin");
+
+//: This is a transformed translation key, the original translation key
+//: (SignIn) is from https://hg.mozilla.org/l10n-central/en-GB/file/0f3c06bd68fc99da1a2e724f5a78119f922a64cc/toolkit/chrome/global/commonDialogs.properties at line 46
+//% "Sign in"
+const auto sign_in = qtTrId("sailfish_components_webview_popups-la-sign_in");
+/* End of translations */
